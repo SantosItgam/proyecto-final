798528a219eece4dab73c1c8fc5f23594a8132da
Santos Vicencio <sntvicencio@gmail.com>
2021-01-25 23:43:55 -0600

    card

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles/styles.css';
+
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize';
+import initDropdown from './helpers/dropdown';
+
+initDropdown.init();
